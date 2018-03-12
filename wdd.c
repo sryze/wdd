c9faabade073841b9a21633a5e55cd0b925482f5
Sergey Zolotarev <sryze01@gmail.com>
2018-03-12 23:28:07 +0600

    Refactoring

--- wdd.c
+++ wdd.c
@@ -24,78 +24,90 @@
 
 #define DEFAULT_INITIAL_BUFFER_SIZE 4096
 
-typedef struct {
+struct program_options {
     const char *filename_in;
     const char *filename_out;
     size_t block_size;
     size_t count;
     const char *status;
-} ProgramOptions;
+};
 
-static HANDLE file_in;
-static HANDLE file_out;
-static BOOL is_writing_to_device = FALSE;
-static size_t buffer_size;
-static char *buffer;
-static BOOL started_copying = FALSE;
-static ULONGLONG start_time;
-static size_t num_bytes_in = 0;
-static size_t num_bytes_out = 0;
-static size_t num_blocks_copied = 0;
+struct program_state {
+    HANDLE in_file;
+    HANDLE out_file;
+    size_t buffer_size;
+    char *buffer;
+    BOOL out_file_is_device;
+    BOOL started_copying;
+    ULONGLONG start_time;
+    size_t num_bytes_in;
+    size_t num_bytes_out;
+    size_t num_blocks_copied;
+};
 
-static void PrintUsageAndExit() {
-    fprintf(stderr, "Usage: wdd if=<in_file> of=<out_file> "
-                              "[bs=N] [count=N] [status=progress]\n");
-    exit(EXIT_FAILURE);
+static void print_usage() {
+    fprintf(stderr, "Usage: wdd if=<in_file> of=<out_file> [bs=N] [count=N] "
+                               "[status=progress]\n");
 }
 
-static ULONGLONG GetSystemTimeMicroseconds() {
+static ULONGLONG get_time_usec() {
     FILETIME filetime;
     ULARGE_INTEGER time;
 
     GetSystemTimeAsFileTime(&filetime);
     time.LowPart = filetime.dwLowDateTime;
     time.HighPart = filetime.dwHighDateTime;
-
     return time.QuadPart / 10;
 }
 
-static void FormatByteCount(char *buffer, size_t buffer_size, double size) {
+static void format_size(char *buffer, size_t buffer_size, size_t size) {
     if (size >= (1 << 30)) {
-        snprintf(buffer, buffer_size, "%0.1f GB", size / (1 << 30));
+        snprintf(buffer, buffer_size, "%0.1f GB", (double)size / (1 << 30));
     } else if (size >= (1 << 20)) {
-        snprintf(buffer, buffer_size, "%0.1f MB", size / (1 << 20));
+        snprintf(buffer, buffer_size, "%0.1f MB", (double)size / (1 << 20));
     } else if (size >= (1 << 10)) {
-        snprintf(buffer, buffer_size, "%0.1f KB", size / (1 << 10));
+        snprintf(buffer, buffer_size, "%0.1f KB", (double)size / (1 << 10));
     } else {
-        snprintf(buffer, buffer_size, "%0.1f bytes", size);
+        snprintf(buffer, buffer_size, "%zu bytes", size);
     }
 }
 
-static void PrintStatus() {
+static void format_speed(char *buffer, size_t buffer_size, double speed) {
+    if (speed >= (1 << 30)) {
+        snprintf(buffer, buffer_size, "%0.1f GB/s", speed / (1 << 30));
+    } else if (speed >= (1 << 20)) {
+        snprintf(buffer, buffer_size, "%0.1f MB/s", speed / (1 << 20));
+    } else if (speed >= (1 << 10)) {
+        snprintf(buffer, buffer_size, "%0.1f KB/s", speed / (1 << 10));
+    } else {
+        snprintf(buffer, buffer_size, "%0.1f bytes/s", speed);
+    }
+}
+
+static void print_status(const struct program_state *s) {
     ULONGLONG elapsed_time;
     double speed;
-    char bytes_str[10];
-    char speed_str[10];
+    char bytes_str[16];
+    char speed_str[16];
 
-    elapsed_time = GetSystemTimeMicroseconds() - start_time;
+    elapsed_time = get_time_usec() - s->start_time;
     if (elapsed_time >= 1000000) {
-        speed = (double)num_bytes_out / ((double)elapsed_time / 1000000);
+        speed = s->num_bytes_out / ((double)elapsed_time / 1000000);
     } else {
-        speed = (double)num_bytes_out;
+        speed = (double)s->num_bytes_out;
     }
 
-    FormatByteCount(bytes_str, sizeof(bytes_str), num_bytes_in);
-    FormatByteCount(speed_str, sizeof(speed_str), speed);
+    format_size(bytes_str, sizeof(bytes_str), s->num_bytes_in);
+    format_speed(speed_str, sizeof(speed_str), speed);
 
-    printf("%zu bytes (%s) copied, %0.1f s, %s/s\n",
-        num_bytes_out,
+    printf("%zu bytes (%s) copied, %0.1f s, %s\n",
+        s->num_bytes_out,
         bytes_str,
         (double)elapsed_time / 1000000.0,
         speed_str);
 }
 
-static char *GetErrorMessage(DWORD error) {
+static char *get_error_message(DWORD error) {
     char *buffer = NULL;
 
     FormatMessageA(
@@ -109,19 +121,22 @@ static char *GetErrorMessage(DWORD error) {
     return buffer;
 }
 
-static void CleanUpOnExit() {
-    VirtualFree(buffer, 0, MEM_RELEASE);
+static void cleanup(const struct program_state *s) {
+    VirtualFree(s->buffer, 0, MEM_RELEASE);
 
-    if (is_writing_to_device) {
-        DeviceIoControl(file_out, FSCTL_UNLOCK_VOLUME,
+    if (s->out_file_is_device) {
+        DeviceIoControl(s->out_file, FSCTL_UNLOCK_VOLUME,
             NULL, 0, NULL, 0, NULL, NULL);
     }
 
-    CloseHandle(file_in);
-    CloseHandle(file_out);
+    CloseHandle(s->in_file);
+    CloseHandle(s->out_file);
 }
 
-static void ExitWithError(int error_code, char *format, ...) {
+static void exit_on_error(const struct program_state *s,
+                          int error_code,
+                          char *format,
+                          ...) {
     va_list arg_list;
     char *reason;
 
@@ -130,19 +145,19 @@ static void ExitWithError(int error_code, char *format, ...) {
     va_end(arg_list);
     fprintf(stderr, ": ");
 
-    reason = GetErrorMessage(error_code);
+    reason = get_error_message(error_code);
     fprintf(stderr, reason);
     LocalFree(reason);
 
-    if (started_copying) {
-        PrintStatus();
+    if (s->started_copying) {
+        print_status(s);
     }
 
-    CleanUpOnExit();
+    cleanup(s);
     exit(EXIT_FAILURE);
 }
 
-static size_t ParseSize(const char *str) {
+static size_t parse_size(const char *str) {
     char *end = NULL;
     size_t size = (size_t)_strtoi64(str, &end, 10);
 
@@ -165,17 +180,20 @@ static size_t ParseSize(const char *str) {
     return size;
 }
 
-static BOOL IsEmptyStringOrNull(const char *s) {
+static BOOL is_empty_string(const char *s) {
     return s == NULL || *s == '\0';
 }
 
-static BOOL CheckOptions(ProgramOptions *options) {
-    return !IsEmptyStringOrNull(options->filename_in)
-        && !IsEmptyStringOrNull(options->filename_out);
-}
-
-static void ParseOptions(int argc, char **argv, ProgramOptions *options) {
+static BOOL parse_options(int argc,
+                          char **argv,
+                          struct program_options *options) {
     int i;
+
+    options->filename_in = NULL;
+    options->filename_out = NULL;
+    options->block_size = 0;
+    options->count = -1;
+    options->status = NULL;
 
     for (i = 1; i < argc; i++) {
         char *value = NULL;
@@ -186,28 +204,38 @@ static void ParseOptions(int argc, char **argv, ProgramOptions *options) {
         } else if (strcmp(name, "of") == 0) {
             options->filename_out = _strdup(value);
         } else if (strcmp(name, "bs") == 0) {
-            options->block_size = ParseSize(value);
+            options->block_size = parse_size(value);
         } else if (strcmp(name, "count") == 0) {
             options->count = (size_t)_strtoi64(value, NULL, 10);
         } else if (strcmp(name, "status") == 0) {
             options->status = _strdup(value);
         }
     }
+
+    return !is_empty_string(options->filename_in)
+        && !is_empty_string(options->filename_out);
 }
 
 int main(int argc, char **argv) {
-    ProgramOptions options = {NULL, NULL, 0, -1};
+    struct program_options options;
+    struct program_state s;
+    size_t num_blocks_copied = 0;
     BOOL show_progress = FALSE;
     DISK_GEOMETRY_EX disk_geometry;
 
-    ParseOptions(argc, argv, &options);
-    if (!CheckOptions(&options)) {
-        PrintUsageAndExit();
+    if (!parse_options(argc, argv, &options)) {
+        print_usage();
+        return 1;
     }
 
-    start_time = GetSystemTimeMicroseconds();
+    s.start_time = get_time_usec();
+    s.out_file_is_device = FALSE;
+    s.started_copying = FALSE;
+    s.num_bytes_in = 0;
+    s.num_bytes_out = 0;
+    s.num_blocks_copied = 0;
 
-    file_in = CreateFileA(
+    s.in_file = CreateFileA(
         options.filename_in,
         GENERIC_READ,
         FILE_SHARE_READ | FILE_SHARE_WRITE,
@@ -215,8 +243,9 @@ int main(int argc, char **argv) {
         OPEN_EXISTING,
         FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,
         NULL);
-    if (file_in == INVALID_HANDLE_VALUE) {
-        ExitWithError(
+    if (s.in_file == INVALID_HANDLE_VALUE) {
+        exit_on_error(
+            &s,
             GetLastError(),
             "Could not open input file or device %s for reading",
             options.filename_in);
@@ -226,7 +255,7 @@ int main(int argc, char **argv) {
      * use OPEN_ALWAYS because it fails when out_file is a physical drive
      * (no idea why).
      */
-    file_out = CreateFileA(
+    s.out_file = CreateFileA(
         options.filename_out,
         GENERIC_WRITE,
         FILE_SHARE_READ | FILE_SHARE_WRITE,
@@ -234,8 +263,8 @@ int main(int argc, char **argv) {
         OPEN_EXISTING,
         FILE_ATTRIBUTE_NORMAL,
         NULL);
-    if (file_out == INVALID_HANDLE_VALUE) {
-        file_out = CreateFileA(
+    if (s.out_file == INVALID_HANDLE_VALUE) {
+        s.out_file = CreateFileA(
             options.filename_out,
             GENERIC_WRITE,
             FILE_SHARE_READ | FILE_SHARE_WRITE,
@@ -244,103 +273,111 @@ int main(int argc, char **argv) {
             FILE_ATTRIBUTE_NORMAL,
             NULL);
     }
-    if (file_out == INVALID_HANDLE_VALUE) {
-        ExitWithError(
+    if (s.out_file == INVALID_HANDLE_VALUE) {
+        exit_on_error(
+            &s,
             GetLastError(),
             "Could not open output file or device %s for writing",
             options.filename_out);
     }
 
-    buffer_size = DEFAULT_INITIAL_BUFFER_SIZE;
-    is_writing_to_device = DeviceIoControl(
-                file_out,
-                IOCTL_DISK_GET_DRIVE_GEOMETRY,
-                NULL,
-                0,
-                &disk_geometry,
-                sizeof(disk_geometry),
-                NULL,
-                NULL);
+    s.buffer_size = DEFAULT_INITIAL_BUFFER_SIZE;
+    s.out_file_is_device = DeviceIoControl(
+        s.out_file,
+        IOCTL_DISK_GET_DRIVE_GEOMETRY,
+        NULL,
+        0,
+        &disk_geometry,
+        sizeof(disk_geometry),
+        NULL,
+        NULL);
 
-    if (is_writing_to_device) {
+    if (s.out_file_is_device) {
         DWORD sector_size;
 
-        if (!DeviceIoControl(file_out, FSCTL_DISMOUNT_VOLUME,
+        if (!DeviceIoControl(s.out_file, FSCTL_DISMOUNT_VOLUME,
                 NULL, 0, NULL, 0, NULL, NULL)) {
-            ExitWithError(GetLastError(), "Failed to dismount output volume");
+            exit_on_error(
+                &s,
+                GetLastError(),
+                "Failed to dismount output volume");
         }
-        if (!DeviceIoControl(file_out, FSCTL_LOCK_VOLUME,
+        if (!DeviceIoControl(s.out_file, FSCTL_LOCK_VOLUME,
                 NULL, 0, NULL, 0, NULL, NULL)) {
-            ExitWithError(GetLastError(), "Failed to lock output volume");
+            exit_on_error(
+                &s,
+                GetLastError(),
+                "Failed to lock output volume");
         }
 
         sector_size = disk_geometry.Geometry.BytesPerSector;
         if (options.block_size < sector_size) {
-            buffer_size = sector_size;
+            s.buffer_size = sector_size;
         } else {
-            buffer_size = (buffer_size / sector_size) * sector_size;
+            s.buffer_size = (s.buffer_size / sector_size) * sector_size;
         }
     } else {
         if (options.block_size > 0) {
-            buffer_size = options.block_size;
+            s.buffer_size = options.block_size;
         }
     }
 
-    buffer = VirtualAlloc(
+    s.buffer = VirtualAlloc(
         NULL,
-        buffer_size,
+        s.buffer_size,
         MEM_COMMIT | MEM_RESERVE,
         PAGE_READWRITE);
-    if (buffer == NULL) {
-        ExitWithError(GetLastError(), "Failed to allocate buffer");
+    if (s.buffer == NULL) {
+        exit_on_error(&s, GetLastError(), "Failed to allocate buffer");
     }
 
     if (options.status != NULL && strcmp(options.status, "progress") == 0) {
         show_progress = TRUE;
     }
 
-    started_copying = TRUE;
+    s.started_copying = TRUE;
 
     for (;;) {
         size_t num_block_bytes_in;
         size_t num_block_bytes_out;
         BOOL result;
 
-        if (options.count >= 0 && num_blocks_copied >= options.count) {
+        if (options.count >= 0 && s.num_blocks_copied >= options.count) {
             break;
         }
 
         result = ReadFile(
-            file_in,
-            buffer,
-            buffer_size,
+            s.in_file,
+            s.buffer,
+            s.buffer_size,
             &num_block_bytes_in,
             NULL);
-        if (!result) {
-            ExitWithError(GetLastError(), "Error reading from file");
-        }
-        num_bytes_in += num_block_bytes_in;
-
-        if (num_block_bytes_in == 0) {
+        if (num_block_bytes_in == 0
+            || (!result && GetLastError() == ERROR_SECTOR_NOT_FOUND)) {
             break;
         }
+        if (!result) {
+            exit_on_error(&s, GetLastError(), "Error reading from file");
+        }
+
+        s.num_bytes_in += num_block_bytes_in;
 
         result = WriteFile(
-            file_out,
-            buffer,
+            s.out_file,
+            s.buffer,
             num_block_bytes_in,
             &num_block_bytes_out,
             NULL);
         if (!result) {
-            ExitWithError(GetLastError(), "Error writing to file");
+            exit_on_error(&s, GetLastError(), "Error writing to file");
         }
-        num_bytes_out += num_block_bytes_out;
 
-        num_blocks_copied++;
+        s.num_bytes_out += num_block_bytes_out;
+        s.num_blocks_copied++;
     }
 
-    CleanUpOnExit();
-    PrintStatus();
+    cleanup(&s);
+    print_status(&s);
 
     return 0;
 }
